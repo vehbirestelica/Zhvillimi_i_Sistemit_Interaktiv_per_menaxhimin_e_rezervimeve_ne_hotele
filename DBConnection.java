a4dab85e234acb23f73d595906c618c441dc156c
TeutaUkshini <44062876+TeutaUkshini@users.noreply.github.com>
2019-06-13 00:14:35 +0300

    Create DBConnection.java

---
+++ DBConnection.java
@@ -0,0 +1,30 @@
+package Employer;
+
+
+import java.sql.*;
+
+public class DBConnection {
+
+	private static Connection dbConnection;
+	
+	private final static String host = "localhost";
+	private final static String dbName = "employer";
+	private final static String username = "root";
+	private final static String password = "";
+	
+	public static Connection getConnection() {
+		if(dbConnection == null) {
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				dbConnection = 
+						DriverManager.getConnection("jdbc:mysql://" + host+ "/" + dbName, username, password);
+				
+			} catch(Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+		
+		return dbConnection;
+	}
+		
+}
